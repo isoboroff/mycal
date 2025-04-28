a37e439fd8ea362f610fcf580ec36dec83535d3c
soboroff <ian.soboroff@nist.gov>
2025-04-28 10:59:28 -0400

    updating get_doc binary to current stuff

--- src/bin/get_doc.rs
+++ src/bin/get_doc.rs
@@ -18,10 +18,14 @@ fn main() -> Result<()> {
     let intid = coll
         .get_doc_intid(&args.docid)
         .expect(&format!("Docid {} not found", &args.docid));
-    let fv: FeatureVec = coll
+    let mut fv: FeatureVec = coll
         .get_fv(intid)
         .expect(&format!("Feature vec not found for docid {}", &args.docid));
-    println!("Doc {} ({}): {:?}", args.docid, intid, fv);
+    println!("Doc {} ({})", args.docid, intid);
+    fv.features.sort_by(|fva, fvb| fva.id.cmp(&fvb.id));
+    fv.features
+        .iter()
+        .for_each(|fp| println!("  {} {}", fp.id, fp.value));
 
     Ok(())
 }
